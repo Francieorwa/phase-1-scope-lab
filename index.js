55bd3813b96e3caab184a8ca69b443c771fa1678
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-23 15:14:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob"
+function customerName(){
+    return customerName;
+}
+
+var customerName = "bob"
+function upperCaseCustomerName(){
+    return customerName =customerName.toUpperCase();
+}
+
+ function setBestCustomer(){
+     bestCustomer = "not bob"
+    
+ }
+ function overwriteBestCustomer(){
+    bestCustomer ="maybe bob"
+ }
+
+ const LeastFavoriteCustomer ="mary"
+ function changeLeastFavoriteCustomer(){
+ LeastFavoriteCustomer ="Jane"
+ }
+
